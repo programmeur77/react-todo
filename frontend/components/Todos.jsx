a80299fc01d783e72bf68aeb013e1283c06bb3fd
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-25 09:59:04 +0100

    Added conditional rendering for some elements and classNames

--- frontend/components/Todos.jsx
+++ frontend/components/Todos.jsx
@@ -1,28 +1,38 @@
 import { FaRegCircle } from "react-icons/fa";
+import { IoIosCheckmarkCircle } from "react-icons/io";
 import { RiDeleteBinLine } from "react-icons/ri";
 
 const Todos = ({ todo, handleIsCompleted, handleDeleteTodo }) => {
-    return (
-      <>
-        <ul className="todo-list__list">
+  return (
+    <>
+      <ul className="todo-list__list">
         <li
-          className="todo-list__item"
-          key={todo.id}
-          onClick={() => handleIsCompleted(todo)}
+          className={
+            todo.completed
+              ? "todo-list__item todo-list__item--completed"
+              : "todo-list__item"
+          }
         >
-          <FaRegCircle
-            className="todolist__item-circle"
-            onClick={() => handleIsCompleted(todo)}
-          />
+          {todo.completed ? (
+            <IoIosCheckmarkCircle
+              className="todo-list__item-completed-icon"
+              onClick={() => handleIsCompleted(todo)}
+            />
+          ) : (
+            <FaRegCircle
+              className="todolist__item-circle"
+              onClick={() => handleIsCompleted(todo)}
+            />
+          )}
           {todo.content}
           <RiDeleteBinLine
             className="todolist__item-remove-icon"
             onClick={() => handleDeleteTodo(todo.id)}
           />
         </li>
-        </ul>
-      </>
-    );
-  };
+      </ul>
+    </>
+  );
+};
 
 export default Todos;
