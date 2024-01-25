6e1e6bb38ec579237cb7f83b5f7b440edefab210
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 00:27:55 +0100

    Implemented Header

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,12 @@
+import { ImCheckboxChecked } from "react-icons/im";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <ImCheckboxChecked className="header__logo" />
+      <h1>Todo App</h1>
+    </header>
+  );
+};
+
+export default Header;
