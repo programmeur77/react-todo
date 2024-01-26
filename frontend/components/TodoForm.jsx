ba8661b90998f147e8e03d34378f8a049717cca3
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 03:26:19 +0100

    Added a wrapper around input and button to place button inside input

--- frontend/components/TodoForm.jsx
+++ frontend/components/TodoForm.jsx
@@ -12,18 +12,20 @@ import "./../style/components/todo-form.scss";
 const TodoForm = ({ newItem, handleOnChange, handleSubmit }) => {
   return (
     <form action="POST" className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="new-item"
-        className="todo-form__input"
-        autoFocus
-        placeholder="Add new item"
-        value={newItem}
-        onChange={(e) => handleOnChange(e.target.value)}
-      />
-      <button className="todo-form__submit-btn" onClick={handleSubmit}>
-        Add
-      </button>
+      <div className="todo-form-wrapper">
+        <input
+          type="text"
+          name="new-item"
+          className="todo-form__input"
+          autoFocus
+          placeholder="Add new item"
+          value={newItem}
+          onChange={(e) => handleOnChange(e.target.value)}
+        />
+        <button className="todo-form__submit-btn" onClick={handleSubmit}>
+          Add
+        </button>
+      </div>
     </form>
   );
 };
