2df89e8bdf802a4ba43d286ffc3be1b2796e5c83
programmeur77 <33042825+programmeur77@users.noreply.github.com>
2024-01-26 01:38:23 +0100

    Added comment on forst line of file to explain what it is + imported SCSS component stylesheet + Removed label element

--- frontend/components/TodoForm.jsx
+++ frontend/components/TodoForm.jsx
@@ -1,9 +1,17 @@
+/** 
+ @description This file contains the implementations of the form which aims at adding Todos
+ @author Benoît Puech
+ @copyright 2024
+ @license Free to use and modify
+ @version 1.0.0
+ ----------------------------------------------------------------
+ LAST UPDATE : 2024 / 01 / 26
+*/
+import "./../style/components/todo-form.scss";
+
 const TodoForm = ({ newItem, handleOnChange, handleSubmit }) => {
   return (
     <form action="POST" className="todo-form" onSubmit={handleSubmit}>
-      <label htmlFor="New item" className="todo-form__input-label">
-        New item
-      </label>
       <input
         type="text"
         name="new-item"
